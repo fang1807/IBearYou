2f9a765933b32c0eecf3b738af69c519993f2e33
Fang <aratitraya.atty@mail.kmutt.ac.th>
2021-05-12 02:17:31 +0700

    Header

---
+++ Home.js
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native'
+import Header from './components/Header'
+
+const Home = () => {
+    return (
+        <View style={styles.container}>
+            <Header/>
+       
+        </View>        
+    )   
+}
+
+
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        alignItems: 'center',
+        paddingTop: 32
+    },
+    text: {
+        color: '#000000',
+        fontSize: 18
+        
+    }
+})
+
+export default Home;
+
