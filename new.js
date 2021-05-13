083a6dc31d4fbdef6f98ee110ac1b50098e66b4b
Fang <aratitraya.atty@mail.kmutt.ac.th>
2021-05-13 23:01:02 +0700

    update poundza007

---
+++ new.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class new extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> test test poundza007 </Text>
+      </View>
+    );
+  }
+}
+
+export default new;
