40a424f353cadca7fd87dc8d06ef223a36368116
Fang <aratitraya.atty@mail.kmutt.ac.th>
2021-05-25 15:28:32 +0700

    so sadddd

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import {AppRegistry} from 'react-native';
 //import darkBg from './components/darkBg';
 //import login from './login';
 //import quiz from './quizScreen';
-import Home from './Home';
+//import Home from './Home';
 //import navigation from './navigation';
 //import welcome from './welcome';
 
@@ -16,7 +16,7 @@ import {name as appName} from './app.json';
 //AppRegistry.registerComponent(appName, () => welcome
 //AppRegistry.registerComponent(appName, () => quiz
 //AppRegistry.registerComponent(appName, () => Bg1
-AppRegistry.registerComponent(appName, () => Home
+//AppRegistry.registerComponent(appName, () => Home
 //AppRegistry.registerComponent(appName, () => login
 //AppRegistry.registerComponent(appName, () => navigation
 
